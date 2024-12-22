5d3d631a01f1bc88e288c5dda8a50d0edb52644c
Worestesa <Worestesa@users.noreply.github.com>
2024-12-22 23:45:44 +0300

    HPbar

--- Assets/Scripts/PlayrHP.cs
+++ Assets/Scripts/PlayrHP.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class PlayrHP : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float maxHP;
+    [SerializeField] private Image HPBar;
 
+    float n;
+    private void Start()
+    {
+        n = maxHP;
+    }
+    public void DPSPlayer(float dps)
+    {
+        n = n - dps;
+        HPBar.fillAmount = n / 100;
+        if (n <= 0)
+        {
+            SceneManager.LoadScene("0");
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
