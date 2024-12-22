e6207f82779c08d2cf5a5d0f6eed44668374acca
Worestesa <Worestesa@users.noreply.github.com>
2024-12-22 23:38:08 +0300

    PlayrControler

--- Assets/Scripts/PlayrController.cs
+++ Assets/Scripts/PlayrController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayrController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float Speed;
+    public Rigidbody2D Body;
+    public float HP;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(Speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(Speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0, Speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, Speed * Time.deltaTime, 0);
+        }
     }
 }
